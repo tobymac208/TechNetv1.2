3b61b18cc5764aaca2e4101fbff0bad7344ce4ae
Nik <ironman_fern@comcast.net>
2018-02-04 11:07:15 -0600

    Updated styling, and various other styling fixes.

--- src/com/groupproject/nik/Box/UserBoxes.java
+++ src/com/groupproject/nik/Box/UserBoxes.java
@@ -2,8 +2,11 @@ package com.groupproject.nik.Box;
 
 import com.groupproject.nik.Model.Account;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -27,7 +30,14 @@ public class UserBoxes {
         Button logoutButton = new Button("Logout of " + currentAccount.getUsername());
         Button settingsButton = new Button("Settings for " + currentAccount.getUsername());
 //      LEFT objects
+        Label optionsLabel = new Label("Window Options");
         Button inventoryButton = new Button("Inventory");
+//      CENTER objects
+        TextArea informationArea = new TextArea();
+        informationArea.setText("Username: " + currentAccount.getUsername() + "\n"
+        + "First name: " + currentAccount.getFirstName() + "\n"
+        + "Last name: " + currentAccount.getLastName() + "\n"
+        + "Age: " + currentAccount.getAge() + "\n");
 
 //      Events
         logoutButton.setOnAction(event -> {
@@ -51,13 +61,17 @@ public class UserBoxes {
         topLayout.getChildren().setAll(logoutButton, settingsButton);
 //      LEFT layout
         VBox leftLayout = new VBox(10);
-        leftLayout.getChildren().addAll(inventoryButton);
+        leftLayout.setAlignment(Pos.CENTER);
+        leftLayout.getChildren().addAll(optionsLabel, inventoryButton);
 //      MAIN layout
         BorderPane.setMargin(topLayout, new Insets(10, 10, 10, 10));
         BorderPane.setMargin(leftLayout, new Insets(10, 10, 10, 10));
+        BorderPane.setMargin(informationArea, new Insets(10, 10, 10, 10));
+
         BorderPane mainLayout = new BorderPane();
         mainLayout.setTop(topLayout);
         mainLayout.setLeft(leftLayout);
+        mainLayout.setCenter(informationArea);
 
 //      Scene options
         Scene mainScene = new Scene(mainLayout, 600, 400);
